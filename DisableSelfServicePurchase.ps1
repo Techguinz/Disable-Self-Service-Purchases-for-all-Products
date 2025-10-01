69e511f5455dd10a220f68c3ffef6d29b6903602
Techguin <136117866+Techguinz@users.noreply.github.com>
2025-10-01 15:35:38 +0100

    Update DisableSelfServicePurchase.ps1

--- DisableSelfServicePurchase.ps1
+++ DisableSelfServicePurchase.ps1
@@ -1,14 +1,21 @@
-﻿<#
-=============================================================================================
-Name:              Disable self-service purchase for Microsoft 365 products
-Description:       This script disables self-service purchase/ trial capability for all the supported M365 products
-Script written by: AdminDroid community team
-=====================================================================================
-#>
+Install-Module -Name MSCommerce # This is responsible for contacting the tenants
+Import-Module MSCommerce
 
-Install-Module -Name MSCommerce -Scope CurrentUser #once you install you should remove this line
-Import-Module -Name MSCommerce 
-Connect-MSCommerce #sign-in with your global or billing administrator account when prompted
-Get-MSCommerceProductPolicies -PolicyId AllowSelfServicePurchase | Where { $_.PolicyValue -eq “Enabled”} | forEach { 
- Update-MSCommerceProductPolicy -PolicyId AllowSelfServicePurchase -ProductId $_.ProductID -Enabled $false  
-}
+do {
+    # Asks for Tenant Details
+    Write-Host "Please sign in to the tenant you want to manage..." -ForegroundColor Cyan
+    Connect-MSCommerce
+
+    # Disable self-service purchase for all products currently enabled
+    $enabledProducts = Get-MSCommerceProductPolicies -PolicyId AllowSelfServicePurchase | Where-Object { $_.PolicyValue -eq "Enabled" }
+
+    foreach ($product in $enabledProducts) {
+        Write-Host "Disabling self-service purchase for product: $($product.ProductName)" -ForegroundColor Yellow
+        Update-MSCommerceProductPolicy -PolicyId AllowSelfServicePurchase -ProductId $product.ProductID -Enabled $false
+    }
+
+    Write-Host "Self-service purchase disabled for all applicable products in this tenant." -ForegroundColor Green
+
+    # Allows you to end the session or move onto another tenant you manage to disable it also
+    $continue = Read-Host "Do you want to manage another tenant? (Y/N)"
+} while ($continue -eq "Y")
